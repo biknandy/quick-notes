393c81e1f4e334383c7bffcc8204c76ee16f2d53
Bik Nandy <68867182+biknandy@users.noreply.github.com>
2024-03-24 00:41:20 -0700

    Description update

--- src/components/exportForm.tsx
+++ src/components/exportForm.tsx
@@ -1,4 +1,4 @@
-import { Form, ActionPanel, Action, Icon, popToRoot, showToast } from "@raycast/api";
+import { Form, ActionPanel, Action, Icon, popToRoot, showToast, useNavigation, Toast } from "@raycast/api";
 import { colors, exportNotes } from "../utils/utils";
 import fs from "fs";
 import { useAtom } from "jotai";
@@ -6,6 +6,7 @@ import { notesAtom } from "../services/atoms";
 
 const ExportForm = () => {
   const [notes, _] = useAtom(notesAtom);
+  const { pop } = useNavigation();
 
   return (
     <Form
@@ -18,6 +19,11 @@ const ExportForm = () => {
               tintColor: colors.find((c) => c.name === "green")?.tintColor,
             }}
             onSubmit={async (values: { folders: string[] }) => {
+              if (notes.length === 0) {
+                showToast({ title: "No Notes to Export", message: "⌘ + N for new note", style: Toast.Style.Failure });
+                pop();
+                return;
+              }
               const folder = values.folders[0];
               if (!fs.existsSync(folder) || !fs.lstatSync(folder).isDirectory()) {
                 return false;
